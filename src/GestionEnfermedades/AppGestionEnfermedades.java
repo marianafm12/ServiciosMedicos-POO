2c0ad161d54b6791eacf51b33a894d3d7a928fff
Robbie Salazar <robbiesalazar@192.168.1.74>
2025-05-06 21:00:08 -0600

    prueba

--- src/GestionEnfermedades/AppGestionEnfermedades.java
+++ src/GestionEnfermedades/AppGestionEnfermedades.java
@@ -4,5 +4,6 @@ public class AppGestionEnfermedades {/*
                                       * public static void main(String[] args) {
                                       * LoginFrame.main(args);
                                       * }
+                                      * hola esto es una prueba
                                       */
 }
